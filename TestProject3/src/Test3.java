afdce733a77a600a53d7ea778172cfcc7d905dba
javatutorkumar <39039313+javatutorkumar@users.noreply.github.com>
2018-05-07 09:19:26 -0500

    check in on central repo

--- TestProject3/src/Test3.java
+++ TestProject3/src/Test3.java
@@ -1,9 +1,8 @@
 
 public class Test3 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) {		
+		System.out.println("git hub 3.1");
 	}
 
 }
